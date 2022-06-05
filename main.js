1a3273de412277ad0df0d00b00764b98c936e091
Abideen Olafimihan <olafimihana@gmail.com>
2022-06-05 21:28:17 +0100

    updating files

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
 // get the DOM elements
-const StudentForm = document.getElementById('studentForm');
+const studentForm = document.getElementById('studentForm');
 const studentContainer = document.querySelector('.students');
+const nameInput = studentForm['name'];
+const ageInput = studentForm['age'];
+const rollInput = studentForm['roll']
+
+//create an empty array to store the students
+
+const students = [];
+
+//create a function that will push student into the array created
+
+const addStudent = ( name, age, roll) => {
+    students.push({
+        name,
+        age,
+        roll
+    })
+
+    return { name, age, roll}
+}
+
+//create a functio that will create students on the DOM
+
+const createStudentElement = () => {
+    //create student data
+    const studentDiv = document.createElement('div');
+    const studentName = document.createElement('h2');
+    const studentAge = document.createElement('p');
+    const studentRoll = document.createElement('p');
+
+    //add text to the created element
+    studentName.innerText = `Student Name: ${name}`;
+    studentAge.innerText = `Student Name: ${age}`;    
+    studentRoll.innerText = `Student Name: ${roll}`;
+
+    // append the create data to student div
+    studentDiv.append(studentName, studentAge, studentRoll);
+    studentContainer.appendChild(studentDiv);
+}
